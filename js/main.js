8233eb52c7d6b21d059b5d0e17270fd8d373f213
Gabriel Maldaner Becker <gabrielbecker8135@gmail.com>
2019-03-15 20:03:11 -0400

    js v2

--- js/main.js
+++ js/main.js
@@ -33,3 +33,64 @@ function comparar() {
       comparar();
     }
   }
+
+  function validar() {
+    var nome = document.getElementById('nome').value;
+    var idade = document.getElementById('idade').value;
+    var cpf = document.getElementById('cpf').value;
+    var telefone = document.getElementById('telefone').value;
+    var cep = document.getElementById('cep').value;
+    var endereco = document.getElementById('endereco').value;
+
+    if (nome == "") {
+      alert("O Nome está vazio. Digite algo...");
+      document.getElementById('nome').focus();
+      return false;
+    }
+
+    if (idade == "") {
+      alert("A Idade está vazia. Digite algo...");
+      document.getElementById('idade').focus();
+      return false;
+    }
+
+    if (isNaN(idade)) {
+      alert("O campo idade só aceita números.");
+    }
+
+    if (cpf == "") {
+      alert("O CPF está vazio. Digite algo...");
+      document.getElementById('cpf').focus();
+      return false;
+    }
+
+    if (isNaN(cpf)) {
+      alert("O cpf idade só aceita números.");
+    }
+
+    if (telefone == "") {
+      alert("O Telefone está vazio. Digite algo...");
+      document.getElementById('telefone').focus();
+      return false;
+    }
+
+    if (isNaN(telefone)) {
+      alert("O telefone idade só aceita números.");
+    }
+
+    if (cep == "") {
+      alert("O CEP está vazio. Digite algo...");
+      document.getElementById('cep').focus();
+      return false;
+    }
+
+    if (isNaN(cep)) {
+      alert("O cep idade só aceita números.");
+    }
+
+    if (endereco == "") {
+      alert("O Endereco está vazio. Digite algo...");
+      document.getElementById('endereco').focus();
+      return false;
+    }
+  }
